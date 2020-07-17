1a31f1908131b423b346c07c81a7a96605259797
Chindalath Traymany <chindalatht@gmail.com>
2020-07-17 08:30:43 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,20 +1,11 @@
-require 'pry'
-
 class EmailAddressParser
   attr_accessor :emails
-  @@all = []
-  
+
   def initialize(emails)
     @emails = emails
   end
   
-  def self.all
-    @@all
-  end
-  
   def parse
-    emails_array = emails.split(/, | /)
-    emails_array.uniq
+    emails.split(/, | /).uniq
   end
 end
-
